829b43c9835638929daae8c66a40dc1d3ba3c393
findcoo <thirdlif2@gmail.com>
2018-02-22 00:48:55 +0900

    Add babel config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-class A {
-    constructor() {
-        this.msg = "hello";
-    }
+import { observable } from "mobx"
+import { observer } from "mobx-react"
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
 
-    get message() {
-        return this.msg;
-    }
+@observer
+class Counter extends Component {
+  @observable count = 0
+
+  render() {
+    return (
+      <div>
+        Counter: {this.count} <br />
+        <button onClick={this.handleInc}> + </button>
+        <button onClick={this.handleDec}> - </button>
+      </div>
+    )
+  }
+
+  handleDec = () => {
+    this.count++
+  }
+
+  handleInc = () => {
+    this.count--
+  }
 }
 
-const a = new A();
-console.log(a.message);
+ReactDOM.render(<Counter />, document.getElementById("root"))
