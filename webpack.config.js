a8dd2f4c1122ddf7b01f2b09f791d721f360981c
findcoo <thirdlif2@gmail.com>
2018-02-21 18:31:29 +0900

    create mobx

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const CleanWebpackPlugin = require("clean-webpack-plugin")
+const webpack = require("webpack")
 
 const config = {
-  entry: "./src/index.js",
+  entry: ["babel-polyfill", "react-hot-loader/patch", "./src/index.js"],
   devtool: "inline-source-map",
   devServer: {
-    contentBase: "./build"
+    contentBase: "./build",
+    hot: true
   },
   output: {
     path: path.resolve(__dirname, "build"),
@@ -17,12 +19,15 @@ const config = {
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "index.html"
-    })
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
       { test: /\.(png|svg|img|gif)$/, use: "file-loader" },
       { test: /\.(woff|woff2|eot|ttf|otf)$/, use: "file-loader" }
     ]
