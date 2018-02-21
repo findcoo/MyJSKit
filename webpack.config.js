e4f1624a79140bf7438f747671d1fc8a4ffeef66
findcoo <thirdlif2@gmail.com>
2018-02-21 17:47:24 +0900

    Update version

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,32 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
 
 const config = {
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './build'
-    },
-    output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-    },
-    plugins: [
-        new CleanWebpackPlugin(['build']),
-        new HtmlWebpackPlugin({ 
-            filename: 'index.html',
-            template: 'index.html'
-        })    
-    ],
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            { test: /\.(png|svg|img|gif)$/, use: 'file-loader' },
-            { test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader' }
-        ]
-    }
-};
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./build"
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new CleanWebpackPlugin(["build"]),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "index.html"
+    })
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(png|svg|img|gif)$/, use: "file-loader" },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: "file-loader" }
+    ]
+  }
+}
 
-module.exports = config;
-
+module.exports = config
